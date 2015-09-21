fcbd1cd75b8a1828ab2fc521a023dbcdd102410c
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-09-20 22:52:55 -0400

    effecient while structure

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Lesson_6_Part_2
     {
         static void Main(string[] args)
         {
+            int count = 0;  // sentinel variable
+
+            while (count != 30) // condition to check
+            {
+                Console.WriteLine($"Playing count: {count}");
+                count++;    //increment by 1
+            }
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Please press any key to exit ...");
+            Console.ReadKey();
+
         }
     }
 }
